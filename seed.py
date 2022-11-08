5db7e4d17754cc39e32634f74a67030e3230ebd9
root <root@Erik-PC.localdomain>
2022-11-08 14:15:10 -0500

    added admin to seed.py

--- seed.py
+++ seed.py
@@ -5,3 +5,14 @@ from app import app
 
 db.drop_all()
 db.create_all()
+
+username='admin'
+password='admin'
+email='erik@erikrichard.com'
+first='Erik'
+last='Richard'
+is_admin=True
+
+admin_user = User.register(username, password, email, first, last, is_admin)
+db.session.add(admin_user)
+db.session.commit()
